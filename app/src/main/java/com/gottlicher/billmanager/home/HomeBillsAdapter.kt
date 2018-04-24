1698a798a3057e3fd916701845f548ae48d06020
Fabio Gottlicher <g.fabio.cz@gmail.com>
2018-04-23 22:16:47 -0600

    example of loading a bill from DB into view

--- app/src/main/java/com/gottlicher/billmanager/home/HomeBillsAdapter.kt
+++ app/src/main/java/com/gottlicher/billmanager/home/HomeBillsAdapter.kt
@@ -3,21 +3,26 @@ package com.gottlicher.billmanager.home
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import com.gottlicher.billmanager.R
 import com.gottlicher.billmanager.model.Bill
+import com.gottlicher.billmanager.utils.inflate
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.item_bill_main.*
 
 /**
  * Created by fabio.gottlicher on 2/19/18.
  */
-class HomeBillsAdapter : RecyclerView.Adapter<HomeBillsAdapter.BillViewHolder>() {
+class HomeBillsAdapter(val items: List<Bill>) : RecyclerView.Adapter<HomeBillsAdapter.BillViewHolder> () {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BillViewHolder {
-        return BillViewHolder(parent);
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = BillViewHolder(parent.inflate(R.layout.item_bill_main))
+
+    override fun onBindViewHolder(holder: BillViewHolder, position: Int) = holder.bind(items[position])
+    override fun getItemCount(): Int = items.size
+
+    class BillViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+        fun bind(bill: Bill) {
+            billName.text = bill.name
+            billDueDate.text = bill.dayOfMonthDue.toString()
+        }
     }
-
-    override fun onBindViewHolder(holder: BillViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-    override fun getItemCount(): Int = 5
-
-    class BillViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
--- app/src/main/java/com/gottlicher/billmanager/home/MainActivity.kt
+++ app/src/main/java/com/gottlicher/billmanager/home/MainActivity.kt
@@ -1,13 +1,21 @@
 package com.gottlicher.billmanager.home
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import com.gottlicher.billmanager.BillManagerApplication
 import com.gottlicher.billmanager.R
 import com.gottlicher.billmanager.model.Bill
 import com.gottlicher.billmanager.model.PastPaidBill
 import io.objectbox.BoxStore
 import io.objectbox.kotlin.boxFor
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
+import org.jetbrains.anko.sdk25.coroutines.onClick
 import org.jetbrains.anko.toast
 import java.util.*
 import javax.inject.Inject
@@ -15,12 +23,46 @@ import kotlinx.coroutines.experimental.android.UI as UI
 
 class MainActivity : AppCompatActivity() {
 
-    @Inject lateinit var objectBox : BoxStore
+    @Inject
+    lateinit var objectBox: BoxStore
+
+    var isLoading:Boolean
+        get() = mainProgress.visibility == View.VISIBLE
+        set(value){
+            mainProgress.visibility = if (value) View.VISIBLE else View.GONE
+            mainBillList.visibility = if(value) View.GONE else View.VISIBLE
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         BillManagerApplication.graph.inject(this)
+
+        mainPlusFab.onClick { onPlusClick() }
+        mainBillList.layoutManager = LinearLayoutManager (this)
+        loadBillsIntoView()
+    }
+
+    private fun loadBillsIntoView() = launch(UI) {
+        isLoading = true
+        delay(500)
+        val allBills = async {
+            val billBox = objectBox.boxFor<Bill>()
+            delay(1000)
+            billBox.all
+        }.await()
+        mainBillList.adapter = HomeBillsAdapter(allBills)
+        mainBillList.adapter.notifyDataSetChanged()
+        isLoading = false
+    }
+
+    private fun onPlusClick() = launch(UI) {
+        loadBillsIntoView()
+//        val intent = Intent(this@MainActivity, AddBillActivity::class.java)
+//        this@MainActivity.startActivity(intent)
+    }
+
+    fun test(){
         var billBox = objectBox.boxFor<Bill>()
 
         var bill = Bill(name = "Pizza", dayOfMonthDue = 25, dayOfMonthAvail = 10, appOrWebName = "PizzaApp")
