5ce8b69406ada64802dec699e5f116b61e20bf63
Fabio Gottlicher <g.fabio.cz@gmail.com>
2018-04-29 21:28:26 -0600

    setting up data binding

--- app/src/main/java/com/gottlicher/billmanager/home/HomeBillsAdapter.kt
+++ app/src/main/java/com/gottlicher/billmanager/home/HomeBillsAdapter.kt
@@ -1,28 +1,34 @@
 package com.gottlicher.billmanager.home
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.gottlicher.billmanager.R
+import com.gottlicher.billmanager.databinding.ItemBillMainBinding
 import com.gottlicher.billmanager.model.Bill
-import com.gottlicher.billmanager.utils.inflate
 import kotlinx.android.extensions.LayoutContainer
-import kotlinx.android.synthetic.main.item_bill_main.*
 
 /**
  * Created by fabio.gottlicher on 2/19/18.
  */
 class HomeBillsAdapter(val items: List<Bill>) : RecyclerView.Adapter<HomeBillsAdapter.BillViewHolder> () {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = BillViewHolder(parent.inflate(R.layout.item_bill_main))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):BillViewHolder {
+        val li = LayoutInflater.from(parent.context);
+        val itemBinding = ItemBillMainBinding.inflate(li, parent, false)
+        return BillViewHolder (itemBinding)
+    }
 
     override fun onBindViewHolder(holder: BillViewHolder, position: Int) = holder.bind(items[position])
     override fun getItemCount(): Int = items.size
 
-    class BillViewHolder(override val containerView: View) : RecyclerView.ViewHolder(containerView), LayoutContainer {
+    class BillViewHolder(val binding: ItemBillMainBinding) : RecyclerView.ViewHolder(binding.root), LayoutContainer {
+        override val containerView: View?
+            get() = binding.root
+
         fun bind(bill: Bill) {
-            billName.text = bill.name
-            billDueDate.text = bill.dayOfMonthDue.toString()
+            binding.bill = bill
+            binding.executePendingBindings()
         }
     }
 }
--- app/src/main/java/com/gottlicher/billmanager/model/model.kt
+++ app/src/main/java/com/gottlicher/billmanager/model/model.kt
@@ -1,5 +1,6 @@
 package com.gottlicher.billmanager.model
 
+import android.databinding.BaseObservable
 import io.objectbox.annotation.Entity
 import io.objectbox.annotation.Id
 import io.objectbox.relation.ToOne
@@ -17,15 +18,24 @@ import java.util.*
         val name: String,
         val dayOfMonthDue: Int,
         val dayOfMonthAvail: Int,
-        val appOrWebName: String){
+        val appOrWebName: String) : BaseObservable (){
     constructor() : this(0,"",0,0,"")
 
     @Backlink  val pastPaid: ToMany<PastPaidBill>? = null
+
+    var _isPaid = false;
+    var isPaidThisMonth:Boolean
+        get() = _isPaid
+        set(value) { _isPaid = value }
+
+    fun getNextDueDateString(): String {
+        return "4/$dayOfMonthDue/2018"
+    }
 }
 
 @Entity class PastPaidBill(
         @Id var id: Long = 0,
-        var paid: Date){
+        var paid: Date) : BaseObservable (){
     constructor() : this(0,Date())
     lateinit var bill: ToOne<Bill>
 }
